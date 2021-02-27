98a02387513ace60e83fe9a8e6b25995e52549e2
crablind1 <crablin@mail.com>
2021-02-27 18:42:26 +0300

    Подключен tinymce

--- places/models.py
+++ places/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from tinymce.models import HTMLField
 
 
 class Place(models.Model):
     title = models.CharField(max_length=200)
     description_short = models.CharField(max_length=255)
-    description_long = models.TextField()
+    description_long = HTMLField()
     coordinates_lat = models.CharField(max_length=20)
     coordinates_lng = models.CharField(max_length=20)
 
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,21 +1,5 @@
-"""where_to_go URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
-from django.shortcuts import render
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from places.views import index, json_place
@@ -24,6 +8,8 @@ from places.views import index, json_place
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tinymce/', include('tinymce.urls')),
     path('', index, name='index_page'),
     path('places/<int:id>/', json_place),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
