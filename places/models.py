89967e27572d779c1ad047828f59c11906ed75e8
crablind1 <Crablin@mail.com>
2021-03-10 20:23:45 +0300

    Добавлен русскоязычный verbose_name

--- places/models.py
+++ places/models.py
@@ -3,11 +3,12 @@ from tinymce.models import HTMLField
 
 
 class Place(models.Model):
-    title = models.CharField(max_length=200)
-    short_description = models.TextField(blank=True)
-    long_description = HTMLField(blank=True)
-    coordinates_lat = models.FloatField()
-    coordinates_lng = models.FloatField()
+    title = models.CharField(max_length=200, verbose_name='Название')
+    short_description = models.TextField(blank=True,
+                                         verbose_name='Короткое описание')
+    long_description = HTMLField(blank=True, verbose_name='Полное описание')
+    coordinates_lat = models.FloatField(verbose_name='Широта')
+    coordinates_lng = models.FloatField(verbose_name='Долгота')
 
     def __str__(self):
         return self.title
@@ -16,7 +17,7 @@ class Place(models.Model):
 class PlaceImage(models.Model):
     place = models.ForeignKey(Place, related_name='images',
                               on_delete=models.CASCADE, verbose_name='Место')
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/', verbose_name='Изображение')
     position = models.PositiveIntegerField(default=0, blank=True,
                                            verbose_name="Позиция")
 
