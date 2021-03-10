45f78d890882c0938cf0831467c3d884e9fbbcaf
crablind1 <Crablin@mail.com>
2021-03-10 22:31:53 +0300

    Отделите STATIC_ROOT от STATICFILES_DIR

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -77,7 +77,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -90,12 +89,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static')
-    ]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "collected_static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
